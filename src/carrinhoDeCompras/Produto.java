9b663f01bd273f393f06a96156439ecce67b2027
unknown <alexandreliberatto@gmail.com>
2024-09-21 15:15:07 -0300

    Criando classe produto

---
+++ src/carrinhoDeCompras/Produto.java
@@ -0,0 +1,25 @@
+package carrinhoDeCompras;
+
+public class Produto {
+
+    private String nome;
+    private double preco;
+
+    public Produto(String nome) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " R$: " + preco;
+    }
+}
