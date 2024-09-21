4c153f99678a181116949c17f92accaca474d113
unknown <alexandreliberatto@gmail.com>
2024-09-21 18:53:19 -0300

    Criando métodos e classe OrdenacaoPessoa
    Signed-off-by: unknown <alexandreliberatto@gmail.com>

---
+++ src/ordenacaoDeListas/OrdenacaoPessoa.java
@@ -0,0 +1,27 @@
+package ordenacaoDeListas;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    //atributo
+    private List<Pessoa> pessoaList;
+
+    //inicia pessoaLista como um array vazio
+    public OrdenacaoPessoa(){
+        this.pessoaList = new ArrayList<>();
+    }
+
+    //adicionando pessoas na lista
+    public void adicionarPessoas(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+}
--- src/ordenacaoDeListas/Pessoa.java
+++ src/ordenacaoDeListas/Pessoa.java
@@ -1,6 +1,8 @@
 package ordenacaoDeListas;
 
-public class Pessoa {
+import java.util.Comparator;
+
+public class Pessoa implements Comparable<Pessoa> {
 
     //atributos
     private String nome;
@@ -12,6 +14,10 @@ public class Pessoa {
         this.nome = nome;
         this.idade = idade;
         this.altura = altura;
+    }
+    @Override
+    public int compareTo(Pessoa p) {
+        return Integer.compare(idade, p.getIdade());
     }
 
     public String getNome() {
@@ -33,5 +39,14 @@ public class Pessoa {
                 ", idade=" + idade +
                 ", altura=" + altura +
                 '}';
+    }
+
+}
+
+class ComparatorPorAltura implements Comparator<Pessoa>{
+
+    @Override
+    public int compare(Pessoa p1, Pessoa p2) {
+        return Double.compare(p1.getAltura(), p2.getAltura());
     }
 }
