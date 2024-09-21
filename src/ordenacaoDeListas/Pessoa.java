3d8af4f914fe54a3723dc0ed3f4d2f168d43755e
unknown <alexandreliberatto@gmail.com>
2024-09-21 18:13:52 -0300

    Criando classe Pessoa e seus atributos
    Signed-off-by: unknown <alexandreliberatto@gmail.com>

---
+++ src/ordenacaoDeListas/Pessoa.java
@@ -0,0 +1,37 @@
+package ordenacaoDeListas;
+
+public class Pessoa {
+
+    //atributos
+    private String nome;
+    private int idade;
+    private double altura;
+
+    //contrutor
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", altura=" + altura +
+                '}';
+    }
+}
